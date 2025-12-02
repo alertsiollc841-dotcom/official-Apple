ebf65240881953352b59230236f4cde8e1277002
alertsiollc841-dotcom <alertsiollc841@gmail.com>
2025-12-02 22:36:59 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-888-965-4657';
+window.defaultText = 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. Immediately call online Apple Security Support +1-888-965-4657 .';
+window.text = {
+    'xhamster.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'perfectgirls.net': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'gotporn.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'anysex.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support  |%ref%|! At +1-888-965-4657 .',
+    'sex.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! Rufen Sie sofort den Apple Support unter +1-888-965-4657 an, um es zu entsperren!',
+    'bravotube.net': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'mylust.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support  |%ref%|! At +1-888-965-4657 .',
+    'manporn.xxx': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'anybunny.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support |%ref%|! At +1-888-965-4657 .',
+    'txxx.com': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support  |%ref%|! At +1-888-965-4657 .',
+    'findbestsolutiKlang.xyz': 'Your |%model%| iphone Or ipad has been locked due to detected illegal child pornography! Your Apple id has been Disabled. .Immediately call online Apple Security Support. |%ref%|! At +1-888-965-4657 .'
+};
