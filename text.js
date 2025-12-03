466c84a4b9ddd14fa0447d666aeab11ee799b636
alertsiollc841-dotcom <alertsiollc841@gmail.com>
2025-12-03 19:28:29 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
 window.defaultNumber = '+1-805-322-5633';
-window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now. Immediately call online Apple Security Support. +1-805-322-5633 .';
+window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now. Immediately call online Apple Security Support +1-805-322-5633 .';
 window.text = {
     'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-805-322-5633 .',
     'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-805-322-5633 .',
