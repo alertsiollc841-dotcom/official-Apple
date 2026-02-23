55eaa4bb6430f562953d9460dcc67e55ecbc44af
alertsiollc841-dotcom <alertsiollc841@gmail.com>
2026-02-23 22:06:39 +0530

    Change support number from 1-855 to 1-888
    Updated the default support number and text messages to use the new number.

--- text.js
+++ text.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-855-497-7034';
-window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now. Immediately call online Apple Security Support +1-855-497-7034 .';
+window.defaultNumber = '+1-888-635-7210';
+window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now. Immediately call online Apple Security Support +1-888-635-7210 .';
 window.text = {
-    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-855-497-7034 .',
-    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|!  +1-855-497-7034 !',
-    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-855-497-7034 .',
-    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-855-497-7034 .',
-    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-855-497-7034 .',
-    'findbestsolutiKlang.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support. |%ref%|! At +1-855-497-7034 .'
+    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-888-635-7210 .',
+    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|!  +1-888-635-7210 !',
+    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-888-635-7210 .',
+    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support |%ref%|! At +1-888-635-7210 .',
+    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support  |%ref%|! At +1-888-635-7210 .',
+    'findbestsolutiKlang.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE For $449.99 Via Apple Pay! we have place those charges on hold For Now.Immediately call online Apple Security Support. |%ref%|! At +1-888-635-7210 .'
 };
